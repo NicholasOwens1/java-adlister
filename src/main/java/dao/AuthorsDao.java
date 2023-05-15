c705f03c174bea61e0b0d861f9fed93124fff5cf
Nicholas Owens <nicholas.s.owens01@gmail.com>
2023-05-15 10:06:37 -0500

    setup

--- src/main/java/dao/AuthorsDao.java
+++ src/main/java/dao/AuthorsDao.java
@@ -1,4 +1,41 @@
 package dao;
 
-public class AuthorsDao implements Authors{
+import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.protocol.Resultset;
+import models.Author;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorsDao implements Authors {
+    private Connection connection;
+    public AuthorsDao() throws SQLException {
+        DriverManager.registerDriver(new Driver());
+        this.connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false",
+                "root",
+                "codeup"
+        );
+    }
+    @Override
+    public List<Author> all() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet rs  = statement.executeQuery("SELECT * FROM authors");
+        List<Author> authors = new ArrayList<>();
+        while (rs.next()) {
+            authors.add(new Author(
+                    rs.getLong("id"),
+                    rs.getString("name")
+            ));
+        }
+        return authors;
+    }
+
+    @Override
+    public Long insert(Author author) {
+        return null;
+    }
+
+
 }
